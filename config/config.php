999caf094819344d9501e4fdca3f9becddc6c3b5
Hassan Jamal <hassanjamal8735@gmail.com>
2026-03-18 15:23:10 +0500

    yesaa

--- config/config.php
+++ config/config.php
@@ -26,7 +26,8 @@ function loadEnv($path) {
 loadEnv(__DIR__ . '/../.env');
 
 // Database settings
-define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
+define('DB_PORT', getenv('DB_PORT') ?: '3306');
 define('DB_USER', getenv('DB_USER') ?: 'root');
 define('DB_PASS', getenv('DB_PASS') ?: '');
 define('DB_NAME', getenv('DB_NAME') ?: 'haya_pharmacy');
